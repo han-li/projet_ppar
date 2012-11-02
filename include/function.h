389a6708ad67feaa671fda2f7213dfa45585c762
LI Han <lhscr8848@hotmail.com>
2012-11-02 23:08:27 +0100

    test fusion

--- include/function.h
+++ include/function.h
@@ -6,6 +6,14 @@
 #include <string.h>
 #include <pthread.h>
 #include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+//#ifdef _OPENMP
+#include <omp.h>
+//#endif
 
 #ifdef DEBUG
 #define DEBUG_PRINT(x,y) printf("%s %d\n",x,y)
@@ -20,6 +28,7 @@
 int sizex,*tabx;
 pthread_t pid[4];
 
+
 /* afficher le tableau */
 void print_tab(int*tab,int size);
 
@@ -28,17 +37,34 @@ int *create_random_tab(int size);
 
 /* si a > b, echange les deux */
 void compare_exchange(int *a,int* b);
+void exchange(int *a,int* b);
 
 /* trier le tab par la facon pair_impair*/
 void sort(int *tab,int size);
 
+/* trier par PRAM */
 void sort2(int *tab,int size);
+
+/* trier par PRAM en openmp */
+void sort_omp(int *tab,int size);
 
 /* verifier le tab soit bien trie ou pas */
 int verify(int *tab,int size);
 
-/* pour tous les donnees de tab1 et tab2, tab1 garde les plus petits, tab2 garde les plus grands */
+/* pour tous les donnees de tab1 et tab2 tries, tab1 garde les plus petits, tab2 garde les plus grands */
 void separate(int **tab1,int **tab2,int size1,int size2);
+
+/* tri_fusion */
+void scinder(int *tab,int size,int **tab2,int *size1,int *size2);
+void fusion(int *tab1,int *tab2,int size1,int size2);
+void trifusion_tmp(int *tab,int size);
+void trifusion(int *tab,int size);
+
+/* tri_rapide */
+int choix_pivot(int *tab,int first,int last);
+int partitionner(int *tab,int first,int last,int pivot);
+void trirapide_tmp(int *tab,int first,int last);
+void trirapide(int *tab,int size);
 
 /* verifier la fonction separate(...) */
 int verify2(int *tab1,int *tab2,int size1,int size2);
--- src/function.c
+++ src/function.c
@@ -25,6 +25,7 @@ int *create_random_tab(int size){
 
 	for(i=0;i<size;i++)
 		tab[i]=(int)rand();
+//		tab[i]=(int)rand()%10;
 
 	return tab;
 }
@@ -32,6 +33,12 @@ int *create_random_tab(int size){
 void compare_exchange(int *a,int* b){
 	if(*a <= *b)
 		return;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void exchange(int *a,int* b){
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
@@ -72,6 +79,49 @@ void sort2(int *tab,int size){
 		while(tab[j]!=-1)j++;
 		tab[j] = new_tab[i];
 	}
+
+	free(ind);
+	free(new_tab);
+}
+
+void sort_omp(int *tab,int size){
+	int *ind = (int*)malloc(sizeof(int)*size);
+       	int *new_tab = (int*)malloc(sizeof(int)*size);
+	int i,j;
+	memcpy(new_tab,tab,sizeof(int)*size);
+
+	omp_set_num_threads(4);
+//	omp_set_nested(1);
+
+#pragma omp parallel
+{
+#pragma omp for private(j)
+	for(i=0;i<size;i++){
+//#pragma omp task private(j)
+//		{
+		ind[i] = 0;
+//#pragma omp parallel
+//#pragma omp for
+		for(j=0;j<size;j++)
+			if(tab[i]>tab[j])
+				ind[i]++;
+		if( i % 10000 == 0 )
+			DEBUG_PRINT("step",i/10000);
+//		}
+	}
+}
+
+	memset(tab,-1,sizeof(int)*size);
+
+#pragma omp parallel
+{
+#pragma omp for private(j)
+	for(i=0;i<size;i++){
+		j=ind[i];
+		while(tab[j]!=-1)j++;
+		tab[j] = new_tab[i];
+	}
+}
 
 	free(ind);
 	free(new_tab);
@@ -128,6 +178,118 @@ void separate(int **tab1,int **tab2,int size1,int size2){
 	*tab2 = new2;
 }	
 
+void scinder(int *tab,int size,int **tab2,int *size1,int *size2){
+	*size1 = size/2;
+	*size2 = size-*size1;
+	
+	*tab2 = &tab[*size1];
+}
+
+void fusion(int *tab1,int *tab2,int size1,int size2){
+	int *new1,*new2,i=0,j=0;
+	new1 = (int*)malloc(sizeof(int)*size1);
+	new2 = (int*)malloc(sizeof(int)*size2);
+	
+	memcpy(new1,tab1,sizeof(int)*size1);
+	memcpy(new2,tab2,sizeof(int)*size2);
+
+
+	while( i+j < size1 ){
+		if( i >= size1 )
+			tab1[i+j] = new2[j++];
+		else if( j >= size2 )
+			tab1[i+j] = new1[i++];
+		else if( new1[i] < new2[j] )
+			tab1[i+j] = new1[i++];
+		else
+			tab1[i+j] = new2[j++];
+		
+	}
+
+	while( i+j < (size1+size2) ){
+		if( i >= size1 )
+			tab2[i+j-size1] = new2[j++];
+		else if( j >= size2 )
+			tab2[i+j-size1] = new1[i++];
+		else if( new1[i] < new2[j] )
+			tab2[i+j-size1] = new1[i++];
+		else
+			tab2[i+j-size1] = new2[j++];
+	}
+
+	free(new1);
+	free(new2);
+}
+
+void trifusion_tmp(int *tab,int size){
+	DEBUG_PRINT("thread",omp_get_thread_num());
+	if(size <= 1)
+		return;
+	
+	int *tab2,size1,size2;
+	
+	scinder(tab,size,&tab2,&size1,&size2);
+#pragma omp task
+	trifusion(tab,size1);
+#pragma omp task
+	trifusion(tab2,size2);
+#pragma omp taskwait
+	fusion(tab,tab2,size1,size2);
+}
+
+void trifusion(int *tab,int size){
+	omp_set_num_threads(4);
+
+#pragma omp parallel
+#pragma omp single
+	trifusion_tmp(tab,size);
+}
+
+int choix_pivot(int *tab,int first,int last){
+	srand(time(NULL));
+	int ind = rand()%(last-first+1) + first;
+	return ind;
+}
+
+int partitionner(int *tab,int first,int last,int pivot){
+	exchange(&tab[last],&tab[pivot]);
+	int i,j = first;
+
+	for(i=first;i<last;i++)
+		if(tab[i]<tab[last]){
+			exchange(&tab[i],&tab[j]);
+			j++;
+		}
+
+	exchange(&tab[j],&tab[last]);
+	return j;
+}
+
+void trirapide_tmp(int *tab,int first,int last){
+	int pivot;
+	if(first<last){
+		DEBUG_PRINT("thread",omp_get_thread_num());
+		pivot = choix_pivot(tab,first,last);
+		DEBUG_PRINT("PIVOT",pivot);
+		//print_tab(&tab[first],last-first+1);
+		pivot = partitionner(tab,first,last,pivot);
+		DEBUG_PRINT("PIVOT",pivot);
+		//print_tab(&tab[first],last-first+1);
+#pragma omp task
+		trirapide_tmp(tab,first,pivot-1);
+#pragma omp task
+		trirapide_tmp(tab,pivot+1,last);
+	}
+}
+
+void trirapide(int *tab,int size){
+	omp_set_num_threads(4);
+#pragma omp parallel
+#pragma omp single
+	trirapide_tmp(tab,0,size-1);
+#pragma omp taskwait
+}
+
 int verify2(int *tab1,int *tab2,int size1,int size2){
 	if(!verify(tab1,size1))
 		return 0;
@@ -172,9 +334,9 @@ void *sort_thread(void* arg){
 
 void separate_thread(int *tab,int size){
 	int i,j,k,l,ssize,sssize;
-	int *tab2 = (int*)malloc(sizeof(int)*size);
+//	int *tab2 = (int*)malloc(sizeof(int)*size);
 
-	memcpy(tab2,tab,sizeof(int)*size);
+//	memcpy(tab2,tab,sizeof(int)*size);
 //	for(i=0;i<size;i++)
 //		tab2[i] = tab[i];
 
@@ -186,8 +348,12 @@ void separate_thread(int *tab,int size){
 		sssize = size - 3*ssize;
 	}
 
-	i=0;j=ssize;k=ssize*2;l=ssize*3;
+//	i=0;j=ssize;k=ssize*2;l=ssize*3;
 
+	fusion(tab,&tab[ssize],ssize,ssize);
+	fusion(&tab[2*ssize],&tab[3*ssize],ssize,sssize);
+	fusion(tab,&tab[2*ssize],2*ssize,ssize+sssize);
+/*
 	DEBUG_PRINT("BEGIN",1);
 	while( i+(j-ssize) < 2*ssize ){
 		if( i >= ssize )
@@ -199,12 +365,12 @@ void separate_thread(int *tab,int size){
 		else
 			tab[i+(j-ssize)] = tab2[i++];
 
-/*		if(!verify(tab,i+j-ssize)){
+		if(!verify(tab,i+j-ssize)){
 			DEBUG_PRINT("i",i);
 			DEBUG_PRINT("j",j);
 			exit(1);
 		}
-*/	}
+	}
 
 	while( k+l-5*ssize < size-2*ssize){
 		if( k >= 3*ssize )
@@ -216,12 +382,12 @@ void separate_thread(int *tab,int size){
 		else
 			tab[k+l-3*ssize] = tab2[k++];
 
-/*		if(!verify(tab+2*ssize,k+l-3*ssize)){
+		if(!verify(tab+2*ssize,k+l-3*ssize)){
 			DEBUG_PRINT("k-1",tab[k-1]);
 			DEBUG_PRINT("l",tab[l]);
 			exit(1);
 		}
-*/	}
+	}
 
 	if(!verify(tab,ssize*2)){
 		DEBUG_PRINT("OH GOD",1);
@@ -247,4 +413,6 @@ void separate_thread(int *tab,int size){
 	}
 
 	free(tab2);
+*/
 }
+
